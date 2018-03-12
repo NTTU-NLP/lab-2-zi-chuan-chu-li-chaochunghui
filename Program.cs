0389246636fd69b2d7a107cbef747366d73d98cf
hui <qwert1997603@gmail.com>
2018-03-12 17:20:48 +0800

    lab2

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\admin\Desktop\新增資料夾\lab-2-zi-chuan-chu-li-chaochunghui-master\Data\Input.txt");
+            String line = null;
+            while((line=file.ReadLine()) != null){
+                var chars = line.ToCharArray();
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if(chars[i]=='.'||chars[i]=='?'||chars[i]=='!'){
+                        Console.WriteLine(chars[i]);
+                    }
+                    else{
+                        Console.Write(chars[i]);        
+                    }
+                    
+                }
+                Console.Read();
+            }
+        }
+    }
+}
